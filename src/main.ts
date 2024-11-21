96391d021ac4463fe948e8a5983ee5f8d63dfcb5
Minsup Ju <me@ptcookie.net>
2024-11-22 00:02:18 +0900

    chore: Project setup
    Add dependencies.
    Apply code format and lint configuration.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,30 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'path';
+import { env, platform } from "node:process";
+import { app, BrowserWindow } from "electron";
+import { join } from "path";
+
+// Forge's Vite plugin constants.
+declare const MAIN_WINDOW_VITE_NAME: string;
+declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
-const createWindow = () => {
+// Add Chromium flag as workaround for Wayland desktop with NVIDIA GPU.
+if (platform === "linux" && env.XDG_SESSION_TYPE === "wayland") {
+  app.commandLine.appendSwitch("ozone-platform-hint", "auto");
+  app.commandLine.appendSwitch("disable-accelerated-video-decode");
+  app.commandLine.appendSwitch("enable-wayland-ime");
+}
+
+function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: join(__dirname, "preload.js"),
     },
   });
 
@@ -20,28 +32,28 @@ const createWindow = () => {
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadFile(join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
-};
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
--- src/renderer.ts
+++ src/renderer.ts
@@ -26,6 +26,7 @@
  * ```
  */
 
-import './index.css';
+import "./app";
+import "./index.css";
 
 console.log('👋 This message is being logged by "renderer.ts", included via Vite');
